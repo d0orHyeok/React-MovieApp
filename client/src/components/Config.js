286703e7db6b5cdfe601fb36b2952d7d38863609
KimJangHyeok <48301140+d0orHyeok@users.noreply.github.com>
2021-10-30 19:54:01 +0900

    Add: LandingPage에 MainImage와 GridCards를 통해 영화 정보 및 이미지 출력

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -2,5 +2,5 @@
 export const USER_SERVER = "/api/users";
 
 export const API_URL = "https://api.themoviedb.org/3/";
-
-export const IMAGE_BASE_URL = "https://image.tndb.org/t/p/";
+export const API_KEY = "83339ed2ae665904f50537397e31ad56";
+export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,14 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { API_URL, API_KEY, IMAGE_BASE_URL } from "../../Config";
+import { Row } from "antd";
+import MainImage from "./Sections/MainImage";
+import GridCards from "../Commons/GridCards";
+import Axios from "axios";
 
 function LandingPage() {
+    const [Movies, setMovies] = useState([]);
+    const [MainMovieImage, setMainMovieImage] = useState("");
+
+    useEffect(() => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+        Axios.get(endpoint).then((response) => {
+            setMovies(response.data.results);
+            setMainMovieImage(response.data.results[0]);
+        });
+    }, []);
+
     return (
-        <>
-            <div className="app">
-                <br />
-                <span style={{ fontSize: "2rem" }}>Let's Start Coding!</span>
+        <div style={{ width: "100%", margin: "0" }}>
+            {/* Main Image */}
+            {MainMovieImage && (
+                <MainImage
+                    title={MainMovieImage.original_title}
+                    desc={MainMovieImage.overview}
+                    image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+                />
+            )}
+            <div style={{ width: "85%", margin: "1rem auto" }}>
+                <h2>Movies by latest</h2>
+                <hr />
+                {/* Movie Grid Cards */}
+                <Row gutter={[10, 10]}>
+                    {Movies &&
+                        Movies.map((movie, index) => (
+                            <React.Fragment>
+                                <GridCards
+                                    image={
+                                        movie.poster_path
+                                            ? `${IMAGE_BASE_URL}w500${movie.poster_path}`
+                                            : null
+                                    }
+                                    movieId={movie.id}
+                                    movieName={movie.original_title}
+                                />
+                            </React.Fragment>
+                        ))}
+                </Row>
             </div>
-            <div style={{ float: "right" }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+                <button>Load More</button>
+            </div>
+        </div>
     );
 }
 
